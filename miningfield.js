a69574d086e2f7cdaa8476f52e64bae3abd45c85
Kalitin Andrey <17819dron@gmail.com>
2017-05-16 20:24:12 +0200

    страница майнинга переведена на конструктор классов, HTML удален. "Страницы" слинкованы. Карта работает как задумано.

---
+++ miningfield.js
@@ -0,0 +1,96 @@
+'use strict'
+
+class Miningfield {
+	constructor(station) {
+		this.station = station;
+		this.miningFieldElement = createAndAppend(document.body, 'div', 'miningField', '');
+		// див карты (см коментарии к карте в station.js)
+		this.mapElement = createAndAppend(document.body, 'div', 'map', '');
+		this.stationButtonElement = createAndAppend(this.mapElement, 'div', 'stationButton', '');
+		this.field_342Element = createAndAppend(this.mapElement, 'div', 'field_342', '');
+		this.mapCloseElement = createAndAppend(this.mapElement, 'div', 'closeMap', 'Закрыть');
+		this.stationButtonElement.onclick = function() {
+			this.station.stationElement.style.display = 'flex';
+			this.miningFieldElement.style.display = 'none';
+			this.mapElement.style.visibility = 'hidden';
+		}.bind(this);
+		this.field_342Element.onclick = function() {
+			this.mapElement.style.visibility = 'hidden';
+		}.bind(this);
+		//левая контрольная панель с меню
+		this.controllPanelElement = createAndAppend(this.miningFieldElement, 'div', 'controllPanel', '');
+		this.courierElement = createAndAppend(this.controllPanelElement, 'div', 'courier', 'Разгрузка');
+		this.overheatElement = createAndAppend(this.controllPanelElement, 'div', 'overheat', 'Перегрев');
+		this.weaponElement = createAndAppend(this.controllPanelElement, 'div', 'weapon', 'Оружие');
+		this.miningLaserElement = createAndAppend(this.controllPanelElement, 'div', 'miningLaser','Буры');
+		this.starMapElement = createAndAppend(this.controllPanelElement, 'div', 'starMap', 'Карта');
+		this.starMapElement.onclick = function() {
+			this.mapElement.style.visibility = 'visible';
+		}.bind(this);
+		this.mapCloseElement.onclick = function() {
+			this.mapElement.style.visibility = 'hidden';
+		}.bind(this);
+		//див поля боя
+		this.battleFieldElement = createAndAppend(this.miningFieldElement, 'div', 'battleField', '');
+		this.locationNameElement = createAndAppend(this.battleFieldElement,	'div',	'locationName',	'Пояс астероидов №342');
+		this.localStatusElement = createAndAppend(this.battleFieldElement,	'div',	'localStatus',	'Безопасный сектор');
+		this.mainFieldElement = createAndAppend(this.battleFieldElement, 'div', 'mainField', '');
+		//корабль игрока на поле
+		this.playersShipElement = createAndAppend(this.mainFieldElement, 'div', 'playersShip', '');
+		//щит корабля
+		this.shieldBorderElement = createAndAppend(this.playersShipElement, 'div', 'shieldBorder', '');
+		this.shieldBarElement = createAndAppend(this.shieldBorderElement, 'div', 'shieldBar', '');
+		this.shieldElement = createAndAppend(this.shieldBorderElement, 'div', 'shield', '30');
+		//броня корабля
+		this.armorBorderElement = createAndAppend(this.playersShipElement, 'div', 'armorBorder', '');
+		this.armorBarElement = createAndAppend(this.armorBorderElement, 'div', 'armorBar', '');
+		this.armorElement = createAndAppend(this.armorBorderElement, 'div', 'armor', '30');
+		//хп корабля
+		this.hpBorderElement = createAndAppend(this.playersShipElement, 'div', 'hpBorder', '');
+		this.hpBarElement = createAndAppend(this.hpBorderElement, 'div', 'hpBar', '');
+		this.HPElement = createAndAppend(this.hpBorderElement, 'div', 'HP', '30');
+		//маленькие астероиды
+		this.smallAsteroid1Element = createAndAppend(this.mainFieldElement, 'div', 'smallAsteroid1', '');
+		//и их ХП
+		this.hpBorderElement = createAndAppend(this.smallAsteroid1Element, 'div', 'hpBorder', '');
+		this.hpBarElement = createAndAppend(this.hpBorderElement, 'div', 'hpBar', '');
+		this.HPElement = createAndAppend(this.hpBorderElement, 'div', 'HP', '30');
+
+		this.smallAsteroid2Element = createAndAppend(this.mainFieldElement, 'div', 'smallAsteroid2', '');
+
+		this.hpBorderElement = createAndAppend(this.smallAsteroid2Element, 'div', 'hpBorder', '');
+		this.hpBarElement = createAndAppend(this.hpBorderElement, 'div', 'hpBar', '');
+		this.HPElement = createAndAppend(this.hpBorderElement, 'div', 'HP', '30');
+
+		this.smallAsteroid3Element = createAndAppend(this.mainFieldElement, 'div', 'smallAsteroid3', '');
+
+		this.hpBorderElement = createAndAppend(this.smallAsteroid3Element, 'div', 'hpBorder', '');
+		this.hpBarElement = createAndAppend(this.hpBorderElement, 'div', 'hpBar', '');
+		this.HPElement = createAndAppend(this.hpBorderElement, 'div', 'HP', '30');
+		//средние астероиды
+		this.mediumAsteroid1Element = createAndAppend(this.mainFieldElement, 'div', 'mediumAsteroid1', '');
+		//и их ХП
+		this.hpBorderElement = createAndAppend(this.mediumAsteroid1Element, 'div', 'hpBorder', '');
+		this.hpBarElement = createAndAppend(this.hpBorderElement, 'div', 'hpBar', '');
+		this.HPElement = createAndAppend(this.hpBorderElement, 'div', 'HP', '30');
+
+		this.mediumAsteroid2Element = createAndAppend(this.mainFieldElement, 'div', 'mediumAsteroid2', '');
+
+		this.hpBorderElement = createAndAppend(this.mediumAsteroid2Element, 'div', 'hpBorder', '');
+		this.hpBarElement = createAndAppend(this.hpBorderElement, 'div', 'hpBar', '');
+		this.HPElement = createAndAppend(this.hpBorderElement, 'div', 'HP', '30');
+		//большой астероид
+		this.bigAsteroid1Element = createAndAppend(this.mainFieldElement, 'div', 'bigAsteroid1', '');
+		//и его ХП
+		this.hpBorderElement = createAndAppend(this.bigAsteroid1Element, 'div', 'hpBorder', '');
+		this.hpBarElement = createAndAppend(this.hpBorderElement, 'div', 'hpBar', '');
+		this.HPElement = createAndAppend(this.hpBorderElement, 'div', 'HP', '30');
+		//див трюма корабля
+		this.holdElement = createAndAppend(this.battleFieldElement, 'div', 'hold', '');
+		this.holdTextElement = createAndAppend(this.holdElement, 'div', 'holdText', 'Загруженность трюма');
+		this.holdBorderElement = createAndAppend(this.holdElement, 'div', 'holdBorder', '');
+		this.holdBarElement = createAndAppend(this.holdBorderElement, 'div', 'holdBar', '');
+		this.holdStatusElement = createAndAppend(this.holdBorderElement, 'div', 'holdStatus', '18000 / 40000');
+	}
+}
+
--- station.js
+++ station.js
@@ -3,72 +3,40 @@
 class Station {
 	constructor() {
 		this.shipmarket = new ShipMarket(this);
+		this.miningfield = new Miningfield(this);
 		// див карты
-		this.mapElement = createAndAppend(
-			document.body, 
-			'div', 
-			'map', 
-			`<a href="index.html">
-				<div class="stationButton"></div>
-			</a>
-			<a href="miningfield.html">
-				<div class="field_342"></div>
-			</a>`
-		);
+		this.mapElement = createAndAppend(document.body, 'div', 'map', '');
+		this.stationButtonElement = createAndAppend(this.mapElement, 'div', 'stationButton', '');
+		this.field_342Element = createAndAppend(this.mapElement, 'div', 'field_342', '');
 		this.mapCloseElement = createAndAppend(this.mapElement, 'div', 'closeMap', 'Закрыть');
-
+		//действия с кнопками карты: скрываем один див, показываем другой
+		this.field_342Element.onclick = function() {
+			this.stationElement.style.display = 'none';
+			this.miningfield.miningFieldElement.style.display = 'flex';
+			this.mapElement.style.visibility = 'hidden';
+		}.bind(this);
+		//действие при нажатии на текущее местоположение
+		this.stationButtonElement.onclick = function() {
+			this.mapElement.style.visibility = 'hidden';
+		}.bind(this);
 		
 		// див станции
 		this.stationElement = createAndAppend(document.body, 'div', 'station', '');
-
 		//див левой контрольной панели
 		this.controllPanelElement = createAndAppend(this.stationElement, 'div', 'controllPanel', '');
 		// элементы меню
-		this.shipMarketElement = createAndAppend(
-			this.controllPanelElement, 
-			'div', 
-			'shipsMarket', 
-			'Корабли'
-		);
-
+		this.shipMarketElement = createAndAppend(this.controllPanelElement, 'div', 'shipsMarket', 'Корабли');
+		//переход на страницу покупки кораблей
 		this.shipMarketElement.onclick = function() {
 			this.stationElement.style.display = 'none';
 			this.shipmarket.shipmarketElement.style.display = 'flex';
 		}.bind(this);
 
-		this.weaponMarketElement = createAndAppend(
-			this.controllPanelElement, 
-			'div', 
-			'weaponsMarket',
-			'Модули'
-		);
-
-
-		this.repairMarketElement = createAndAppend(
-			this.controllPanelElement, 
-			'div', 
-			'repairMarket',
-			'Ремонт'
-		);
-		this.sellMarketElement = createAndAppend(
-			this.controllPanelElement, 
-			'div', 
-			'sellMarket',
-			'Продажа'
-		);
-		this.inventoryElement = createAndAppend(
-			this.controllPanelElement, 
-			'div', 
-			'inventory',
-			'Инвентарь'
-		);
-
-		this.starMapElement = createAndAppend(
-			this.controllPanelElement, 
-			'div', 
-			'starMap',
-			'Карта'
-		);
+		this.weaponMarketElement = createAndAppend(this.controllPanelElement, 'div', 'weaponsMarket','Модули');
+		this.repairMarketElement = createAndAppend(this.controllPanelElement, 'div', 'repairMarket','Ремонт');
+		this.sellMarketElement = createAndAppend(this.controllPanelElement, 'div', 'sellMarket','Продажа');
+		this.inventoryElement = createAndAppend(this.controllPanelElement, 'div', 'inventory','Инвентарь');
+		this.starMapElement = createAndAppend(this.controllPanelElement, 'div', 'starMap','Карта');
 
 		//ошибка: выдавало style of undefiend
 		//решение: .bind(this)
@@ -80,43 +48,16 @@ class Station {
 			this.mapElement.style.visibility = 'hidden';
 		}.bind(this);
 		//див ангара
-		this.hungarContainerElement = createAndAppend(
-			this.stationElement, 
-			'div', 
-			'hungarContainer',
-			''
-		);
-		this.stationNameElement = createAndAppend(
-			this.hungarContainerElement, 
-			'div', 
-			'stationName',
-			'Станция: Земляне люминатые'
-		);
+		this.hungarContainerElement = createAndAppend(this.stationElement, 'div', 'hungarContainer','');
+		this.stationNameElement = createAndAppend(this.hungarContainerElement, 'div', 'stationName','Станция: Земляне люминатые');
 		//корабль игрока
-		this.playersShipElement = createAndAppend(
-			this.hungarContainerElement, 
-			'div', 
-			'playersShip',
-			''
-		);
-		this.weaponPlace1Element = createAndAppend(
-			this.playersShipElement, 
-			'div', 
-			'weaponPlace1',
-			''
-		);
-		this.weaponPlace2Element = createAndAppend(
-			this.playersShipElement, 
-			'div', 
-			'weaponPlace2',
-			''
-		);
-		this.inventoryContainerElement = createAndAppend(
-			this.hungarContainerElement, 
-			'div', 
-			'inventoryContainer',
-			''
-		);
+		this.playersShipElement = createAndAppend(this.hungarContainerElement, 'div', 'playersShip','');
+		this.weaponPlace1Element = createAndAppend(this.playersShipElement, 'div', 'weaponPlace1','');
+		this.weaponPlace2Element = createAndAppend(this.playersShipElement, 'div', 'weaponPlace2','');
+
+		//инвентарь
+		this.inventoryContainerElement = createAndAppend(this.hungarContainerElement, 'div', 'inventoryContainer','');
+
 		for(var i = 0; i < 48; i++){
 			this.inventorySlotElement = createAndAppend(
 				this.inventoryContainerElement, 
@@ -132,7 +73,7 @@ class Station {
 		}.bind(this);
 	}
 
-
+	// изменение стиля инвентаря видимый-невидимый
 	inventoryAppear() {
 		if (this.inventoryContainerElement.style.opacity == 0) {
 			this.inventoryContainerElement.style.opacity = 1;
